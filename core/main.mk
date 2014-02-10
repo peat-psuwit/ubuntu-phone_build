f3e8e9a7dd2546b05d98ab1b041ffde5017d9e2b
Ricardo Salveti de Araujo <ricardo.salveti@canonical.com>
2014-02-10 00:48:57 -0200

    main.mk: Also adding qemu pc-bios when building the x86 emulator
    Change-Id: I24924ee5b03cd4bde9065f2c4babb92f30ec528b
    Signed-off-by: Ricardo Salveti de Araujo <ricardo.salveti@canonical.com>

--- core/main.mk
+++ core/main.mk
@@ -527,6 +527,11 @@ subdirs := \
 	system/security/keystore \
 	system/security/softkeymaster
 
+ifeq ($(TARGET_DEVICE),generic_x86)
+subdirs += \
+	prebuilts/qemu-kernel/x86/pc-bios
+endif
+
 # device and vendor
 subdirs += \
 	device/asus \
